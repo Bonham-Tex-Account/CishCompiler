5bd87fe80fbde050e4ef53cbcb51f7d357524d8d
Tex.Bonham <Tex.Bonham@GMRDL1>
2025-06-12 21:17:29 -0700

    Add array sum function, guessing game, and operator token
    - Started the creation of sample code
    
    - Added `OperatorToken` class in `OperatorToken.cs` within the `CishCompiler.Tokens` namespace, including a constructor and a placeholder for the `RegexPattern` property.

---
+++ CishCompiler/Tokens/OperatorToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CishCompiler.Tokens
+{
+    public class OperatorToken : Token
+    {
+        public override string RegexPattern => throw new NotImplementedException();
+
+        public OperatorToken(string value, int lineNumber) : base(value, lineNumber)
+        {
+        }
+
+    }
+}
